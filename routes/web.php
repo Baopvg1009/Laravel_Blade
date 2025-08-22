6173d7235e2b036c41d2d2af993928111e2fd363
Baopvg1009 <phambao10092003@gmail.com>
2025-08-23 01:02:12 +0700

    Add Login UI

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\SigninController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SignupController;
 Route::get('/', function () {
     return view('home.index');
 });
 Route::get('/signup',[SignupController::class,'create'])->name('signup');
+Route::get('/login',[SigninController::class,'create'])->name('login');
